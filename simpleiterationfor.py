f3b986c8562d461b170cfd44327d9079ba2f5383
godumuyiwa <ishaqgbo@gmail.com>
2019-02-02 13:26:10 +0000

    easy method

--- simpleiterationfor.py
+++ simpleiterationfor.py
@@ -4,7 +4,7 @@ def simpleiterusingfor(a):
         #using a for loop without any accuracy set
         x_new = (2*x**2 + 3)/5
         print(loops, x_new)
-        if x_new == x:
+        if abs(x_new - x) <0.00001:
             break
         else:
             x = x_new
