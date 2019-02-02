7742abdd4941c4be35bba5254cc10c7fcc2c535a
godumuyiwa <ishaqgbo@gmail.com>
2019-02-02 13:31:34 +0000

    easy method

---
+++ simpleiterationfor-a.py
@@ -0,0 +1,14 @@
+from math import *
+def simpleiterusingfor(a):
+    x = a
+    for loops in range (1,101):
+        #using a for loop without any accuracy set
+        x_new = sqrt((3+5*x)/5)
+        print(loops, x_new)
+        if x_new == x:
+            break
+        else:
+            x = x_new
+    print(f"The number of iteration is {loops} and the root is {x_new}")
+
+simpleiterusingfor(100000)
