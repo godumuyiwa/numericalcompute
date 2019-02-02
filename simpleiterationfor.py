a94d15ecd2fade938a6fe1d877c78decc3984c70
godumuyiwa <ishaqgbo@gmail.com>
2019-02-02 13:13:38 +0000

    easy method

--- simpleiterationfor.py
+++ simpleiterationfor.py
@@ -1,6 +1,7 @@
 def simpleiterusingfor(a):
     x = a
     for loops in range (1,101):
+        #using a for loop without any accuracy set
         x_new = (2*x**2 + 3)/5
         print(loops, x_new)
         if x_new == x:
